de9158190a0f1421c6bf577c0f97d75cd7467378
Mike Jensen <mjensen@rbaconsulting.com>
2017-11-10 16:15:12 -0600

    Initial experimental checkin

---
+++ HACDashApp/Controllers/GradesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngularJSWebApiEmpty.Controllers
+{
+    public class GradesController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetGrades(string userId, string password)
+        {
+            dynamic grade = new
+            {
+                G1 = userId,
+                G2 = password
+            };
+
+            return Json(grade);
+        }
+    }
+}
---
+++ HACDashApp/Models/Assignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSWebApiEmpty.Models
+{
+    public class Assignment
+    {
+        public string Description { get; set; }
+        public DateTime DateDue { get; set; }
+        public string Course { get; set; }
+        public string Category { get; set; }
+        public string Points { get; set; }
+        public string Score { get; set; }   // "L" Is bad, loser.
+    }
+}
